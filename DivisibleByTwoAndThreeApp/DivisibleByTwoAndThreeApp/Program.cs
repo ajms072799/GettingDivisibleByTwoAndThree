bb9f45fb52054ac08cc76920771c671b4fe9280e
Aron James delos Reyes <ajms.072799@gmail.com>
2020-10-10 21:37:06 +0800

    added the template file for the application

---
+++ DivisibleByTwoAndThreeApp/DivisibleByTwoAndThreeApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisibleByTwoAndThreeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Getting the divisible by 2 and 3 application
+        }
+    }
+}
