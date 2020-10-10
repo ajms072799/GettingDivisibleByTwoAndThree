08a71b69ecef127bf260575cd78aa2fe6f4c93bf
Aron James delos Reyes <ajms.072799@gmail.com>
2020-10-10 21:44:05 +0800

    added username input and variable for storing user input

--- DivisibleByTwoAndThreeApp/DivisibleByTwoAndThreeApp/Program.cs
+++ DivisibleByTwoAndThreeApp/DivisibleByTwoAndThreeApp/Program.cs
@@ -11,6 +11,20 @@ namespace DivisibleByTwoAndThreeApp
         static void Main(string[] args)
         {
             // Getting the divisible by 2 and 3 application
+
+            // Header of the application
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("\tGetting the Divisible by 2 and 3");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------");
+            Console.Write("What's your name? "); // Getting the user name.
+            string username = Console.ReadLine(); // after the input of the user name it will store in the username variable.
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("--------------------------------------------------");
+            
+            // <~ End of the Getting The Divisible by 2 and 3 App
+            Console.ReadLine();
         }
     }
 }
